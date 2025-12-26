700243471570e68324766fb7a106169111d1cd6e
nicov51 <nicolas.viennot@laposte.net>
2025-12-26 18:13:06 +0100

    feature: optimize images headerHero and ZoneIntervention

--- src/components/HeaderHero.tsx
+++ src/components/HeaderHero.tsx
@@ -2,6 +2,7 @@
 
 import { Box } from "@mui/material";
 import Image from "next/image";
+import headerHeroImage from "@/public/images/headerHero.webp";
 
 export default function HeaderHero() {
   return (
@@ -16,20 +17,19 @@ export default function HeaderHero() {
         role="banner"
     >
       <Image
-        src="/images/headerHero.png"
+        src={headerHeroImage}
         alt="Plafond tendu moderne dans un salon lumineux"
         width={1920}
         height={800}
         style={{ objectFit: "cover", width: "100%", height: "auto" }}
         priority
-        quality={90}  // ✅ Haute qualité pour image hero
-        sizes="100vw" // ✅ Prend toute la largeur
-        placeholder="blur"  // ✅ Effet de flou pendant chargement
-        blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/8QAFQEBAQAAAAAAAAAAAAAAAAAAAAX/xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwCwAB//2Q=="
-        onError={(e) => {
-          console.error("Erreur de chargement de l'image :", e)
-          e.currentTarget.src = '/images/realisation1.webp' // Image de secours
-        }}
+        quality={85}  //  Haute qualité pour image hero
+        sizes="100vw" // Prend toute la largeur
+        placeholder="blur"  // Effet de flou pendant chargement ( blurDataURL generé par next.js)
+        // onError={(e) => {
+        //   console.error("Erreur de chargement de l'image :", e)
+        //   e.currentTarget.src = '/images/realisation1.webp' // Image de secours
+        // }}
       />
     </Box>
   );
